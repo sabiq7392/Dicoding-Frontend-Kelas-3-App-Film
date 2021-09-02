9448f6a06a9cd94472a1b85bd0f017425e810ccd
Sabiq Muhammad <72848275+sabiq7392@users.noreply.github.com>
2021-09-03 01:40:25 +0700

    test ignore

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module",
+        "ignorePatterns": ["eslintrc.js"]
+    },
+    "rules": {
+    },
+    "parser": "babel-eslint",
+};
