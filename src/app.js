c16f0859d9918e3d74dc994e5787a8d5afd2f92e
Sabiq Muhammad <72848275+sabiq7392@users.noreply.github.com>
2021-09-04 01:42:53 +0700

    Delete src directory

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import "regenerator-runtime";
-
-window.addEventListener("load", () => {
-    import("./script/view/main.js");
-    import("./style/css/index.css");
-    import("bootstrap-icons/font/bootstrap-icons.css");
-});
